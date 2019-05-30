ad1b5299fecde29aaa60195915314641adca8127
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-05-29 21:36:18 -0600

    game complete needs style

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
-var removedArr = []
 
 var box1 = document.getElementById("box_1");
 var box2 = document.getElementById("box_2");
@@ -10,10 +9,8 @@ var box6 = document.getElementById("box_6");
 var box7 = document.getElementById("box_7");
 var box8 = document.getElementById("box_8");
 var box9 = document.getElementById("box_9");
+var allBoxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
-var topRow = document.getElementsByClassName("top_row");
-var middleRow = document.getElementsByClassName("middle_row");
-var bottomRow = document.getElementsByClassName("bottom_row");
 var choices = document.getElementsByClassName("choice")
 
 var userChoice = [];
@@ -32,96 +29,192 @@ var winner8 = ["box_3", "box_5", "box_7"];
 function userWinner1Fun() {
   if (userChoice.includes(winner1[0]) && userChoice.includes(winner1[1]) && userChoice.includes(winner1[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner2Fun() {
   if (userChoice.includes(winner2[0]) && userChoice.includes(winner2[1]) && userChoice.includes(winner2[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner3Fun() {
   if (userChoice.includes(winner3[0]) && userChoice.includes(winner3[1]) && userChoice.includes(winner3[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner4Fun() {
   if (userChoice.includes(winner4[0]) && userChoice.includes(winner4[1]) && userChoice.includes(winner4[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner5Fun() {
   if (userChoice.includes(winner5[0]) && userChoice.includes(winner5[1]) && userChoice.includes(winner5[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner6Fun() {
   if (userChoice.includes(winner6[0]) && userChoice.includes(winner6[1]) && userChoice.includes(winner6[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner7Fun() {
   if (userChoice.includes(winner7[0]) && userChoice.includes(winner7[1]) && userChoice.includes(winner7[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function userWinner8Fun() {
   if (userChoice.includes(winner8[0]) && userChoice.includes(winner8[1]) && userChoice.includes(winner8[2])) {
     alert("you win");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 // Computer Wins______________________________________________________
 function compWinner1Fun() {
   if (compPick.includes(winner1[0]) && compPick.includes(winner1[1]) && compPick.includes(winner1[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner2Fun() {
   if (compPick.includes(winner2[0]) && compPick.includes(winner2[1]) && compPick.includes(winner2[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner3Fun() {
   if (compPick.includes(winner3[0]) && compPick.includes(winner3[1]) && compPick.includes(winner3[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner4Fun() {
   if (compPick.includes(winner4[0]) && compPick.includes(winner4[1]) && compPick.includes(winner4[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner5Fun() {
   if (compPick.includes(winner5[0]) && compPick.includes(winner5[1]) && compPick.includes(winner5[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner6Fun() {
   if (compPick.includes(winner6[0]) && compPick.includes(winner6[1]) && compPick.includes(winner6[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner7Fun() {
   if (compPick.includes(winner7[0]) && compPick.includes(winner7[1]) && compPick.includes(winner7[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
 function compWinner8Fun() {
   if (compPick.includes(winner8[0]) && compPick.includes(winner8[1]) && compPick.includes(winner8[2])) {
     alert("Computer Wins");
+    for (var i = 0; i < allBoxes.length; i++) {
+      allBoxes[i].innerHTML = ""
+    }
+    userChoice = [];
+    compPick = [];
+    choiceArr = ["box_1", "box_2", "box_3", "box_4", "box_5", "box_6", "box_7", "box_8", "box_9"]
   }
 }
 
@@ -130,23 +223,24 @@ for (var i = 0; i < choices.length; i++) {
   choices[i].addEventListener("click", function() {
     var userPick = this.id;
     for( var i = 0; i < choiceArr.length; i++){ 
-      if ( choiceArr[i] === userPick) {
+      if (choiceArr[i] === userPick) {
         choiceArr.splice(i, 1);
-        removedArr.push(userPick);
-      }
+        userChoice.push(this.id);
+        var x = document.getElementById("x");
+        var copyX = x.cloneNode(true);
+        document.getElementById(this.id).appendChild(copyX);
+        compChoice = choiceArr[Math.floor(Math.random() * choiceArr.length)]
+        for( var i = 0; i < choiceArr.length; i++){ 
+          if ( choiceArr[i] === compChoice) {
+            choiceArr.splice(i, 1);
+            compPick.push(compChoice);
+            var o = document.getElementById("o")
+            var copyO = o.cloneNode(true)
+            document.getElementById(compChoice).appendChild(copyO)
+          }
+        }
+      } 
     }
-    userChoice.push(this.id);
-    
-    compChoice = choiceArr[Math.floor(Math.random() * choiceArr.length)]
-    for( var i = 0; i < choiceArr.length; i++){ 
-      if ( choiceArr[i] === compChoice) {
-        choiceArr.splice(i, 1);
-        removedArr.push(compChoice);
-        compPick.push(compChoice);
-      }
-    }
-    alert(`you chose ${this.id}`);
-    console.log(userChoice);
     userWinner1Fun();
     userWinner2Fun();
     userWinner3Fun();
@@ -166,7 +260,6 @@ for (var i = 0; i < choices.length; i++) {
     compWinner8Fun();
   })
 }
-
 
 
 
